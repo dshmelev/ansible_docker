28f87b96706e4061d5fdc9d9372171ab4978bb03
Dmitry Shmelev <avikez@gmail.com>
2017-12-14 11:56:14 +0300

    Add entrypoint

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+set -e
 
 render() {
-sedStr="
-  s!%%ANSIBLE_VERSION%%!$version!g;
-"
-
-sed -r "$sedStr" $1
+  sedStr="s!%%ANSIBLE_VERSION%%!$ANSIBLE_VERSION!g;"
+  sed -r "$sedStr" $1
 }
 
 
 mkdir -p .build/$ANSIBLE_VERSION
 render Dockerfile.template > .build/$ANSIBLE_VERSION/Dockerfile
+cp entrypoint.sh .build/$ANSIBLE_VERSION/entrypoint.sh
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -t $DOCKER_REPO:$ANSIBLE_VERSION .build/$ANSIBLE_VERSION
 docker push $DOCKER_REPO:$ANSIBLE_VERSION
